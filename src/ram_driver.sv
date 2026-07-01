044989fb6ed2af6cf8f5b3ec51c4fd342d706548
Jonathan Ezekiels <jonathanezekiels@feserver.mirafra.com>
2026-07-01 15:59:25 +0530

    Updated Driver

--- src/ram_driver.sv
+++ src/ram_driver.sv
@@ -22,14 +22,18 @@ class ram_driver;
 			drv_trans = new();
 			gen_2_drv.get(drv_trans);
 			if(vif.reset == 1) begin
+				repeat (1) @(vif.drv_b);
 				vif.data_in <= 'hz;
 				vif.address <= 0;
 				vif.write_enb <= 0;
 				vif.read_enb <= 0;
 			end
 			else begin
+				repeat(1) @(vif.drv_cb);
 				vif.data_in <= drv_trans.data_in;
 				vif.address <= drv_trans.address;
-
-		
-
+				vif.write_enb <= drv_trans.write_enb;
+				vif.read_enb <= drv_trans.read_enb;
+				drv_2_ref.put(drv_trans);
+				$display("DRIVER to INTERFACE DATA_IN = %0H | ADDR = %0h | WRITE_ENB = %0b | READ_ENB = %0b",drv_trans.data_in,drv_trans.address,drv_trans.write_enb,drv_trans.read_enb);
+				
